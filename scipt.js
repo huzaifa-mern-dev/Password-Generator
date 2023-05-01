2e5f3dedbb0c384a18caca63dd7c4a812a13a052
Muhammad Huzaifa <121051659+Huzaifa3737@users.noreply.github.com>
2023-05-02 03:07:05 +0500

    Add files via upload

---
+++ scipt.js
@@ -0,0 +1,56 @@
+let inputSlider = document.getElementById("inputSlider");
+let sliderValue = document.getElementById("sliderValue");
+let passBox = document.getElementById("passBox");
+let lowercase = document.getElementById("lowercase");
+let uppercase = document.getElementById("uppercase");
+let numbers = document.getElementById("numbers");
+let symbols = document.getElementById("symbols");
+let genBtn = document.getElementById("genBtn");
+let copyIcon = document.getElementById("copyIcon");
+
+//showing input slider.................
+sliderValue.textContent = inputSlider.value;
+inputSlider.addEventListener("input", () => {
+  sliderValue.textContent = inputSlider.value;
+});
+
+genBtn.addEventListener("click", () => {
+  passBox.value = generatePassword();
+});
+
+//function................
+let lowerChars = "abcdefghijklmnopqrstuvwxyz";
+let upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let allNumbers = "0123456789";
+let allSymbols = "!@#$%^&*+";
+
+function generatePassword() {
+  let genPassword = "";
+  let allChars = "";
+
+  allChars += lowercase.checked ? lowerChars : "";
+  allChars += uppercase.checked ? upperChars : "";
+  allChars += numbers.checked ? allNumbers : "";
+  allChars += symbols.checked ? allSymbols : "";
+  if (allChars == "" || allChars.length == "") {
+    return genPassword;
+  }
+
+  let i = 1;
+  while (i <= inputSlider.value) {
+    genPassword += allChars.charAt(Math.floor(Math.random() * allChars.length));
+    i++;
+  }
+  return genPassword;
+}
+
+//copy icon...............
+copyIcon.addEventListener("click", () => {
+  if (passBox.value != "" || passBox.value.length >= 1) {
+    navigator.clipboard.writeText(passBox.value);
+    copyIcon.className = "fa-solid fa-check";
+    setTimeout(() => {
+      copyIcon.className = "fa-solid fa-copy";
+    }, 2000);
+  }
+});
